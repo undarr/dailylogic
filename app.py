318b99f7f1324cca44a94b416abee84197d609c3
Ulfred Chan <101036368+undarr@users.noreply.github.com>
2026-07-17 00:04:02 +0800

    Fix clue extraction and update display method

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ def fetch_robot_data():
 
 result = fetch_robot_data()
 clue=result['result']["capturedTexts"]["clue"]
-dl="()big()".split(clue)[2].replace("(dlnewline)","\n")
+dl=clue.split("()big()")[2].replace("(dlnewline)","\n")
 st.markdown(dl)
+st.latex(dl)
 
 
